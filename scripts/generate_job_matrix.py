5f0a6d01b4e2a4980f155c6ed9eb4dbbd5532be2
Sara Damiano <sdamiano@stroudcenter.org>
2024-06-05 11:11:41 -0400

    scripts for building examples
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

---
+++ scripts/generate_job_matrix.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python
+# %%
+import json
+import shutil
+import re
+from typing import List
+from platformio.project.config import ProjectConfig
+import re
+import os
+import copy
+from pathlib import Path
+
+# %%
+# Some working directories
+
+# The workspace directory
+if "GITHUB_WORKSPACE" in os.environ.keys():
+    workspace_dir = os.environ.get("GITHUB_WORKSPACE")
+else:
+    fileDir = os.path.dirname(os.path.realpath("__file__"))
+    workspace_dir = os.path.join(fileDir, "../")
+    workspace_dir = os.path.abspath(os.path.realpath(workspace_dir))
+
+# The examples directory
+examples_dir = "./examples/"
+examples_path = os.path.join(workspace_dir, examples_dir)
+examples_path = os.path.abspath(os.path.realpath(examples_path))
+
+# The continuous integration directory
+ci_dir = "./continuous_integration/"
+ci_path = os.path.join(workspace_dir, ci_dir)
+ci_path = os.path.abspath(os.path.realpath(ci_path))
+
+# A directory of files to save and upload as artifacts to use in future jobs
+artifact_dir = os.path.join(
+    os.path.join(workspace_dir, "continuous_integration_artifacts")
+)
+if not os.path.exists(artifact_dir):
+    os.makedirs(artifact_dir)
+
+compilers = ["Arduino CLI", "PlatformIO"]
+
+# %%
+# read configurations based on existing files and environment variables
+
+# Arduino CLI configurations
+if "GITHUB_WORKSPACE" in os.environ.keys():
+    arduino_cli_config = os.path.join(workspace_dir, "arduino_cli.yaml")
+else:
+    arduino_cli_config = os.path.join(ci_dir, "arduino_cli_local.yaml")
+
+# PlatformIO configurations
+pio_config_file = os.path.join(ci_path, "platformio.ini")
+have_pio_config_file = False
+if os.path.isfile(pio_config_file):
+    have_pio_config_file = True
+    pio_config = ProjectConfig(pio_config_file)
+    env_to_boards = {}
+    for pio_env_name in pio_config.envs():
+        env_to_boards[pio_env_name] = pio_config.get(
+            "env:{}".format(pio_env_name), "board"
+        )
+    boards = list(env_to_boards.values())
+elif "BOARDS_TO_BUILD" in os.environ.keys():
+    boards = os.environ.get("BOARDS_TO_BUILD").split(",")
+else:
+    boards = ["mayfly"]
+
+# %%
+# Get the examples to build
+if "EXAMPLES_TO_BUILD" in os.environ.keys():
+    non_menu_examples = os.environ.get("EXAMPLES_TO_BUILD").split(",")
+else:
+    # The massive "menu" example
+    menu_example_name = "menu_a_la_carte"
+    menu_file_path = os.path.join(
+        os.path.join(examples_path, menu_example_name), menu_example_name + ".ino"
+    )
+
+    # Find all of the non-menu examples
+    non_menu_examples = [
+        f
+        for f in os.listdir(examples_path)
+        if os.path.isdir(os.path.join(examples_path, f))
+        and f not in [".history", "logger_test", menu_example_name]
+    ]
+
+# %%
+# helper functions to create commands
+
+# Translation between board names on PlatformIO and the Arduino CLI
+pio_to_acli = {
+    # EnviroDIY boards
+    "mayfly": {"fqbn": "EnviroDIY:avr:envirodiy_mayfly"},
+    # Official Arduino AVR boards
+    "uno": {"fqbn": "arduino:avr:uno"},
+    "megaatmega2560": {"fqbn": "arduino:avr:mega"},
+    "leonardo": {"fqbn": "arduino:avr:leonardo"},
+    # Official Arduino SAM boards
+    "dueUSB": {"fqbn": "arduino:sam:arduino_due_x"},
+    # Official Arduino SAMD boards
+    "zeroUSB": {"fqbn": "arduino:samd:mzero_bl"},
+    "mkrvidor4000": {"fqbn": "arduino:samd:mkrvidor4000"},
+    # Official Arduino Mega AVR boards
+    "nano_every": {"fqbn": "arduino:megaavr:nona4809"},
+    "uno_wifi_rev2": {"fqbn": "arduino:megaavr:uno2018"},
+    # Official Arduino ESP32 boards
+    "arduino_nano_esp32": {"fqbn": "arduino:esp32:nano_nora"},
+    # Official Arduino Rasberry Pi boards
+    "pico": {"fqbn": "arduino:mbed_rp2040:pico"},
+    # Official Arduino Renesas boards
+    "uno_r4_wifi": {"fqbn": "arduino:renesas_uno:unor4wifi"},
+    # Adafruit boards
+    "adafruit_feather_m0": {"fqbn": "adafruit:samd:adafruit_feather_m0"},
+    "adafruit_feather_m4": {"fqbn": "adafruit:samd:adafruit_feather_m4"},
+    "adafruit_grandcentral_m4": {"fqbn": "adafruit:samd:adafruit_grandcentral_m4"},
+    # Espressif Boards
+    "nodemcuv": {"fqbn": "esp8266:esp8266:nodemcuv"},
+    "nodemcuv2": {"fqbn": "esp8266:esp8266:nodemcuv2"},
+    "esp32dev": {"fqbn": "esp32:esp32:esp32"},
+    "esp32-c6-devkitc-1": {"fqbn": "esp32:esp32:esp32c6"},
+    "esp32-c3-devkitm-1": {"fqbn": "esp32:esp32:esp32c3"},
+    "esp32-s3-devkitm-1": {"fqbn": "esp32:esp32:esp32s3"},
+}
+
+
+def get_example_folder(subfolder_name):
+    return os.path.join(examples_path, subfolder_name)
+
+
+def get_example_filepath(subfolder_name):
+    ex_folder = get_example_folder(subfolder_name)
+    ex_file = os.path.join(ex_folder, subfolder_name + ".ino")
+    return ex_file
+
+
+def create_arduino_cli_command(code_subfolder: str, pio_board: str) -> str:
+    arduino_command_arguments = [
+        "arduino-cli",
+        "compile",
+        # "--verbose",
+        "--warnings",
+        "more",
+        "--config-file",
+        arduino_cli_config,
+        "--format",
+        "text",
+        "--fqbn",
+        pio_to_acli[pio_config.get("env:{}".format(pio_board), "board")]["fqbn"],
+        os.path.join(examples_path, code_subfolder),
+    ]
+    return " ".join(arduino_command_arguments)
+
+
+def create_pio_ci_command(
+    code_subfolder: str,
+    pio_env: str,
+    pio_env_file: str = pio_config_file,
+    have_pio_config_file: bool = True,
+) -> str:
+    if have_pio_config_file:
+        pio_command_args = [
+            "pio",
+            "ci",
+            # "--verbose",
+            "--project-conf",
+            pio_env_file,
+            "--environment",
+            pio_env,
+            os.path.join(examples_path, code_subfolder),
+        ]
+    else:
+        pio_command_args = [
+            "pio",
+            "ci",
+            # "--verbose",
+            "--board",
+            pio_env,
+            os.path.join(examples_path, code_subfolder),
+        ]
+    return " ".join(pio_command_args)
+
+
+def add_log_to_command(command: str, group_title: str) -> List:
+    command_list = []
+    command_list.append("\necho ::group::{}".format(group_title))
+    command_list.append(command + " 2>&1 | tee output.log")
+    command_list.append("result_code=${PIPESTATUS[0]}")
+    command_list.append(
+        'if [ "$result_code" -eq "0" ]; then echo " - {title} :white_check_mark:" >> $GITHUB_STEP_SUMMARY; else echo " - {title} :x:" >> $GITHUB_STEP_SUMMARY; fi'.format(
+            title=group_title
+        )
+    )
+    command_list.append(
+        'if [ "$result_code" -eq "0" ] && [ "$status" -eq "0" ]; then status=0; else status=1; fi'
+    )
+    command_list.append("echo ::endgroup::")
+    command_list.append(
+        'if [ "$result_code" -eq "0" ]; then echo -e "\e[32m{title} successfully compiled\e[0m"; else echo -e "\e[31m{title} failed to compile\e[0m"; fi'.format(
+            title=group_title
+        )
+    )
+    return command_list
+
+
+def create_logged_command(
+    compiler: str,
+    group_title: str,
+    code_subfolder: str,
+    pio_env: str,
+    pio_env_file: str = pio_config_file,
+):
+    output_commands = []
+    lower_compiler = compiler.lower().replace(" ", "").strip()
+    if lower_compiler == "platformio":
+        build_command = create_pio_ci_command(
+            code_subfolder=code_subfolder, pio_env=pio_env, pio_env_file=pio_env_file
+        )
+    elif lower_compiler == "arduinocli":
+        build_command = create_arduino_cli_command(
+            code_subfolder=code_subfolder,
+            pio_board=pio_env,
+        )
+    else:
+        build_command = ""
+
+    command_with_log = add_log_to_command(build_command, group_title)
+    output_commands.extend(command_with_log)
+
+    return copy.deepcopy(output_commands)
+
+
+# %%
+# set up outputs
+arduino_job_matrix = []
+pio_job_matrix = []
+start_job_commands = "status=0"
+end_job_commands = "\n\nexit $status"
+
+# %%
+# Create job info for the basic examples
+# Use one job per board with one command per example
+for example in non_menu_examples:
+    arduino_ex_commands = [
+        start_job_commands,
+    ]
+    pio_ex_commands = [
+        start_job_commands,
+    ]
+
+    for pio_env in pio_config.envs():
+        for compiler, command_list in zip(
+            compilers, [arduino_ex_commands, pio_ex_commands]
+        ):
+            command_list.extend(
+                create_logged_command(
+                    compiler=compiler,
+                    group_title=pio_env,
+                    code_subfolder=example,
+                    pio_env=pio_env,
+                )
+            )
+
+    arduino_job_matrix.append(
+        {
+            "job_name": "Arduino - {}".format(example),
+            "command": "\n".join(arduino_ex_commands + [end_job_commands]),
+        }
+    )
+    pio_job_matrix.append(
+        {
+            "job_name": "Platformio - {}".format(example),
+            "command": "\n".join(pio_ex_commands + [end_job_commands]),
+        }
+    )
+
+
+# %%
+# Convert commands in the matrix into bash scripts
+for matrix_job in arduino_job_matrix + pio_job_matrix:
+    bash_file_name = matrix_job["job_name"].replace(" ", "") + ".sh"
+    bash_out = open(os.path.join(artifact_dir, bash_file_name), "w+")
+    bash_out.write("#!/bin/bash\n\n")
+    bash_out.write(
+        "# Makes the bash script print out every command before it is executed, except echo\n"
+    )
+    bash_out.write(
+        "trap '[[ $BASH_COMMAND != echo* ]] && echo $BASH_COMMAND' DEBUG\n\n"
+    )
+    bash_out.write(matrix_job["command"])
+    bash_out.close()
+    matrix_job["script"] = os.path.join(artifact_dir, bash_file_name)
+
+# Remove the command from the dictionaries before outputting them
+for items in arduino_job_matrix + pio_job_matrix:
+    if "command" in items:
+        del items["command"]
+
+
+# %%
+# Write out output
+print(
+    'echo "arduino_job_matrix={}" >> $GITHUB_OUTPUT'.format(
+        json.dumps(arduino_job_matrix)
+    )
+)
+json_out = open(os.path.join(artifact_dir, "arduino_job_matrix.json"), "w+")
+json.dump(arduino_job_matrix, json_out, indent=2)
+json_out.close()
+
+
+print('echo "pio_job_matrix={}" >> $GITHUB_OUTPUT'.format(json.dumps(pio_job_matrix)))
+json_out = open(os.path.join(artifact_dir, "pio_job_matrix.json"), "w+")
+json.dump(pio_job_matrix, json_out, indent=2)
+json_out.close()
+
+
+# %%
+# different attempt to save output
+with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+    print("arduino_job_matrix={}".format(json.dumps(arduino_job_matrix)), file=fh)
+    print("pio_job_matrix={}".format(json.dumps(pio_job_matrix)), file=fh)
+
+
+# %%
+if "GITHUB_WORKSPACE" not in os.environ.keys():
+    try:
+        shutil.rmtree(artifact_dir)
+    except:
+        pass
+
+# %%
---
+++ scripts/install-platforms-arduino-cli.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Makes the bash script print out every command before it is executed, except echo
+trap '[[ $BASH_COMMAND != echo* ]] && echo $BASH_COMMAND' DEBUG
+
+# Exit with nonzero exit code if anything fails
+set -e
+
+echo "\n\e[32mCurrent Arduino CLI version:\e[0m"
+arduino-cli version
+
+echo "\n\e[32mUpdating the core index\e[0m"
+arduino-cli --config-file arduino_cli.yaml core update-index
+
+echo "::group::Installing EnviroDIY Cores"
+echo "\n\e[32mInstalling the EnviroDIY AVR Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install EnviroDIY:avr
+echo "::endgroup::"
+
+echo "::group::Installing Arduino Official Cores"
+echo "\n\e[32mInstalling the Arduino AVR Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:avr
+
+echo "\n\e[32mInstalling the Arduino Mega AVR Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:megaavr
+
+echo "\n\e[32mInstalling the Arduino SAMD Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:samd
+
+echo "\n\e[32mInstalling the Arduino ESP32 Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:esp32
+
+echo "\n\e[32mInstalling the Arduino RPi Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:mbed_rp2040
+
+echo "\n\e[32mInstalling the Arduino Renesas Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:renesas_uno
+echo "::endgroup::"
+
+echo "::group::Installing Adafruit Cores"
+echo "\n\e[32mInstalling the Adafruit SAMD Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install adafruit:samd
+echo "::endgroup::"
+
+echo "::group::Installing Espressif Cores"
+echo "\n\e[32mInstalling the ESP8266 Arduino Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install esp8266:esp8266
+
+echo "\n\e[32mInstalling the ESP32 Arduino Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install esp32:esp32
+echo "::endgroup::"
+
+echo "::group::Installing RPi Cores"
+echo "\n\e[32mInstalling the Raspberry Pi Pico Arduino Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install rp2040:rp2040
+echo "::endgroup::"
+
+echo "::group::Installing STM32 Cores"
+echo "\n\e[32mInstalling the STM32 Arduino Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install stm32:stm32
+echo "::endgroup::"
+
+echo "\n\e[32mUpdating the core index\e[0m"
+arduino-cli --config-file arduino_cli.yaml core update-index
+
+echo "\n\e[32mUpgrading all cores\e[0m"
+arduino-cli --config-file arduino_cli.yaml core upgrade
+
+echo "\n\e[32mCurrently installed cores:\e[0m"
+arduino-cli --config-file arduino_cli.yaml core list
