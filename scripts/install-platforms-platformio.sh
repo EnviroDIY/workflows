b42ad5f06f7abbab36adae7fa4e47ebb879e4b89
Sara Damiano <sdamiano@stroudcenter.org>
2024-06-08 19:53:29 -0400

    Don't install libraries with platforms for pio
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- scripts/install-platforms-platformio.sh
+++ scripts/install-platforms-platformio.sh
@@ -32,7 +32,6 @@ pio pkg install -g --tool tool-bossac
 pio pkg install -g --tool toolchain-gccarmnoneeabi
 echo "::endgroup::"
 
-
 echo "\n\e[32mInstalling the ESP8266 Arduino Core\e[0m"
 pio pkg install -g --platform espressif8266
 
@@ -47,103 +46,6 @@ pio pkg install -g --platform ststm32
 
 echo "\n\e[32mInstalling the Teensy Core\e[0m"
 pio pkg install -g --platform teensy
-
-
-echo "::group::Installing Libraries"
-echo "\e[32m\nCurrently installed packages:\e[0m"
-pio pkg list -g -v
-
-echo "\e[32mInstalling envirodiy/EnviroDIY_DS3231\e[0m"
-pio pkg install -g --library envirodiy/EnviroDIY_DS3231
-
-echo "\e[32mInstalling arduino-libraries/RTCZero\e[0m"
-pio pkg install -g --library arduino-libraries/RTCZero
-
-echo "\e[32mInstalling greygnome/EnableInterrupt\e[0m"
-pio pkg install -g --library greygnome/EnableInterrupt
-
-echo "\e[32mInstalling greiman/SdFat\e[0m"
-pio pkg install -g --library greiman/SdFat
-
-echo "\e[32mInstalling vshymanskyy/TinyGSM\e[0m"
-pio pkg install -g --library vshymanskyy/TinyGSM
-
-echo "\e[32mInstalling knolleary/PubSubClient\e[0m"
-pio pkg install -g --library knolleary/PubSubClient
-
-echo "\e[32mInstalling adafruit/'Adafruit BusIO'\e[0m"
-pio pkg install -g --library adafruit/'Adafruit BusIO'
-
-echo "\e[32mInstalling adafruit/'Adafruit Unified Sensor'\e[0m"
-pio pkg install -g --library adafruit/'Adafruit Unified Sensor'
-
-echo "\e[32mInstalling https://github.com/soligen2010/Adafruit_ADS1X15.git\e[0m"
-pio pkg install -g --library https://github.com/soligen2010/Adafruit_ADS1X15.git
-
-echo "\e[32mInstalling adafruit/'Adafruit AM2315'\e[0m"
-pio pkg install -g --library adafruit/'Adafruit AM2315'
-
-echo "\e[32mInstalling adafruit/'Adafruit BME280 Library'\e[0m"
-pio pkg install -g --library adafruit/'Adafruit BME280 Library'
-
-echo "\e[32mInstalling adafruit/'DHT sensor library'\e[0m"
-pio pkg install -g --library adafruit/'DHT sensor library'
-
-echo "\e[32mInstalling adafruit/'Adafruit INA219'\e[0m"
-pio pkg install -g --library adafruit/'Adafruit INA219'
-
-echo "\e[32mInstalling adafruit/'Adafruit MPL115A2'\e[0m"
-pio pkg install -g --library adafruit/'Adafruit MPL115A2'
-
-echo "\e[32mInstalling adafruit/'Adafruit SHT'\e[0m"
-pio pkg install -g --library adafruit/'Adafruit SHT4x Library'
-
-echo "\e[32mInstalling Martin Lindupp's BMP388 Library\e[0m"
-pio pkg install -g --library MartinL1/BMP388_DEV
-
-echo "\e[32mInstalling paulstoffregen/OneWire\e[0m"
-pio pkg install -g --library paulstoffregen/OneWire
-
-echo "\e[32mInstalling milesburton/DallasTemperature\e[0m"
-pio pkg install -g --library milesburton/DallasTemperature
-
-echo "\e[32mInstalling envirodiy/SDI-12\e[0m"
-pio pkg install -g --library envirodiy/SDI-12
-
-echo "\e[32mInstalling northernwidget/MS5803\e[0m"
-pio pkg install -g --library northernwidget/MS5803
-
-echo "\e[32mInstalling https://github.com/EnviroDIY/Tally_Library.git#Dev_I2C\e[0m"
-pio pkg install -g --library https://github.com/EnviroDIY/Tally_Library.git#Dev_I2C
-
-echo "\e[32mInstalling envirodiy/SensorModbusMaster\e[0m"
-pio pkg install -g --library envirodiy/SensorModbusMaster
-
-echo "\e[32mInstalling envirodiy/${GITHUB_REPOSITORY#*/}\e[0m"
-pio pkg install -g --library envirodiy/${GITHUB_REPOSITORY#*/}
-
-echo "\e[32mInstalling envirodiy/YosemitechModbus\e[0m"
-pio pkg install -g --library envirodiy/YosemitechModbus
-
-echo "\e[32mInstalling envirodiy/GroPointModbus\e[0m"
-pio pkg install -g --library envirodiy/GroPointModbus
-
-echo "\e[32mInstalling vshymanskyy/StreamDebugger\e[0m"
-pio pkg install -g --library vshymanskyy/StreamDebugger
-
-echo "\e[32mInstalling https://github.com/PaulStoffregen/AltSoftSerial.git\e[0m"
-pio pkg install -g --library https://github.com/PaulStoffregen/AltSoftSerial.git
-
-echo "\e[32mInstalling https://github.com/Testato/SoftwareWire.git#v1.5.1\e[0m"
-pio pkg install -g --library https://github.com/Testato/SoftwareWire.git#v1.5.1
-
-echo "\e[32mInstalling https://github.com/SRGDamia1/NeoSWSerial.git\e[0m"
-pio pkg install -g --library https://github.com/SRGDamia1/NeoSWSerial.git
-
-echo "\e[32mInstalling https://github.com/EnviroDIY/SoftwareSerial_ExternalInts.git\e[0m"
-pio pkg install -g --library https://github.com/EnviroDIY/SoftwareSerial_ExternalInts.git
-echo "::endgroup::"
-
 
 echo "::group::Current globally installed packages"
 echo "\e[32m\nCurrently installed packages:\e[0m"
