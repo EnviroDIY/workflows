651e2a5018f29f68be9838c3616af3f7486ea5c5
Sara Damiano <sdamiano@stroudcenter.org>
2024-06-20 13:12:48 -0400

    Add platforms, upgrade platforms, change escapes
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- scripts/install-platforms-arduino-cli.sh
+++ scripts/install-platforms-arduino-cli.sh
@@ -8,63 +8,98 @@ if [ "$RUNNER_DEBUG" = "1" ]; then
     set -x # Print command traces before executing command.
 fi
 
-echo "\n\e[32mCurrent Arduino CLI version:\e[0m"
+echo "\e[32mCurrent Arduino CLI version:\e[0m"
 arduino-cli version
 
-echo "\n\e[32mUpdating the core index\e[0m"
+echo "\e[32mUpdating the core index\e[0m"
 arduino-cli --config-file arduino_cli.yaml core update-index
 
 echo "::group::Installing EnviroDIY Cores"
-echo "\n\e[32mInstalling the EnviroDIY AVR Core\e[0m"
+echo "\e[32mInstalling the EnviroDIY AVR Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install EnviroDIY:avr
 echo "::endgroup::"
 
-echo "::group::Installing Arduino Official Cores"
-echo "\n\e[32mInstalling the Arduino AVR Core\e[0m"
+echo "::group::Arduino AVR"
+echo "\e[32mInstalling the Arduino AVR Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install arduino:avr
+echo "::endgroup::"
 
-echo "\n\e[32mInstalling the Arduino Mega AVR Core\e[0m"
-arduino-cli --config-file arduino_cli.yaml core install arduino:megaavr
-
-echo "\n\e[32mInstalling the Arduino SAM Core\e[0m"
+echo "::group::Arduino SAM"
+echo "\e[32mInstalling the Arduino SAM Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install arduino:sam
+echo "::endgroup::"
 
-echo "\n\e[32mInstalling the Arduino SAMD Core\e[0m"
+echo "::group::Arduino SAMD"
+
+echo "\e[32mInstalling the Arduino SAMD Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install arduino:samd
+echo "::endgroup::"
 
-echo "\n\e[32mInstalling the Arduino ESP32 Core\e[0m"
+echo "::group::Arduino MegaAVR"
+echo "\e[32mInstalling the Arduino Mega AVR Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:megaavr
+echo "::endgroup::"
+
+echo "::group::Arduino ESP32"
+echo "\e[32mInstalling the Arduino ESP32 Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install arduino:esp32
+echo "::endgroup::"
 
-echo "\n\e[32mInstalling the Arduino RPi Core\e[0m"
+echo "::group::Arduino Raspberry Pi Pico"
+echo "\e[32mInstalling the Arduino RPi Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install arduino:mbed_rp2040
+echo "::endgroup::"
 
-echo "\n\e[32mInstalling the Arduino Renesas Core\e[0m"
+echo "::group::Arduino Renesas UNO"
+echo "\e[32mInstalling the Arduino Renesas Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install arduino:renesas_uno
 echo "::endgroup::"
 
-echo "::group::Installing Adafruit Cores"
-echo "\n\e[32mInstalling the Adafruit SAMD Core\e[0m"
+echo "::group::Arduino NRF52 UNO"
+echo "\e[32mInstalling the Arduino NRF52 Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:nrf52
+echo "::endgroup::"
+
+echo "::group::Arduino mBed 32"
+echo "\e[32mInstalling the Arduino mBed 32 Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install arduino:mbed_portenta
+echo "::endgroup::"
+
+echo "::group::Adafruit AVR"
+echo "\e[32mInstalling the Adafruit AVR Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install adafruit:avr
+echo "::endgroup::"
+
+echo "::group::Adafruit SAMD"
+echo "\e[32mInstalling the Adafruit SAMD Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install adafruit:samd
 echo "::endgroup::"
 
-echo "::group::Installing Espressif Cores"
-echo "\n\e[32mInstalling the ESP8266 Arduino Core\e[0m"
-arduino-cli --config-file arduino_cli.yaml core install esp8266:esp8266
+echo "::group::Adafruit NRF52"
+echo "\e[32mInstalling the Adafruit NRF52 Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install adafruit:nrf52
+echo "::endgroup::"
 
-echo "\n\e[32mInstalling the ESP32 Arduino Core\e[0m"
+echo "::group::Espressif ESP8266"
+echo "\e[32mInstalling the ESP8266 Arduino Core\e[0m"
+arduino-cli --config-file arduino_cli.yaml core install esp8266:esp8266
+echo "::endgroup::"
+
+echo "::group::Espressif ESP32"
+echo "\e[32mInstalling the ESP32 Arduino Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install esp32:esp32
 echo "::endgroup::"
 
-echo "::group::Installing RPi Cores"
-echo "\n\e[32mInstalling the Raspberry Pi Pico Arduino Core\e[0m"
+echo "::group::Teensy"
+echo "\e[32mInstalling the Teensy Core\e[0m"
 arduino-cli --config-file arduino_cli.yaml core install rp2040:rp2040
 echo "::endgroup::"
 
-echo "\n\e[32mUpdating the core index\e[0m"
+echo "\e[32mUpdating the core index\e[0m"
 arduino-cli --config-file arduino_cli.yaml core update-index
 
-echo "\n\e[32mUpgrading all cores\e[0m"
+echo "\e[32mUpgrading all cores\e[0m"
 arduino-cli --config-file arduino_cli.yaml core upgrade
 
-echo "\n\e[32mCurrently installed cores:\e[0m"
+echo "\e[32mCurrently installed cores:\e[0m"
 arduino-cli --config-file arduino_cli.yaml core list
