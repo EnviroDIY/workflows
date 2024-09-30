f34d6c86b0857ad0e63f8484538d73148a84b808
Sara Damiano <sdamiano@stroudcenter.org>
2024-09-30 17:48:49 -0400

    Change file name in prepare release
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- pioScripts/dump_dependency_search.py
+++ pioScripts/dump_dependency_search.py
@@ -43,9 +43,12 @@ env.Append(CXXFLAGS=["/DEBUG"])
 
 print("Working on environment (PIOENV) {}".format(env["PIOENV"]))
 
-print("\n\nEnviroment Dump:")
-print(env.Dump())
-print("\n\n")
+output_file_name = f"{env['PROJECT_DIR']}\\pio_dependency_dump.log"
+output_file = open(output_file_name, "w")
+
+output_file.write("Enviroment Dump:\n")
+output_file.write(env.Dump())
+output_file.write("\n\n")
 
 shared_lib_dir = env["LIBSOURCE_DIRS"][0]
 shared_lib_abbr = "lib"
@@ -55,44 +58,47 @@ library_json_file = f"{env['PROJECT_DIR']}\\library.json"
 examples_deps_file = f"{env['PROJECT_DIR']}\\examples\\example_dependencies.json"
 proj_config = env.GetProjectConfig()
 lsds = env.GetLibSourceDirs()
-print("\nlsds")
-print(lsds)
-print("\nproject ")
+output_file.write("lsds\n")
+output_file.write(json.dumps(lsds))
+output_file.write("\nproject ")
 project = ProjectAsLibBuilder(env, "$PROJECT_DIR")
-print(project)
-print("LDF Mode:", project.lib_ldf_mode)
-print("SRC DIR:", project.src_dir)
-print("\nSRC Filter:", json.dumps(project.src_filter, indent=2))
-print("\nDependencies:", project.dependencies)
-print("\nInitial dependency builders:")
-print(project.depbuilders)
+output_file.write(str(project))
+output_file.write("\nLDF Mode:")
+output_file.write(project.lib_ldf_mode)
+output_file.write("\nSRC DIR:")
+output_file.write(project.src_dir)
+output_file.write("\nSRC Filter:")
+output_file.write(json.dumps(project.src_filter, indent=2))
+output_file.write("\nDependencies:\n")
+output_file.write(json.dumps(project.dependencies))
+output_file.write("\nInitial dependency builders:\n")
+output_file.write(str(project.depbuilders))
 
 lib_builders = env.GetLibBuilders()
-print("\n\nFound %d compatible libraries" % len(lib_builders))
-print(lib_builders)
-print()
+output_file.write("\n\nFound %d compatible libraries\n" % len(lib_builders))
+output_file.write(str(lib_builders))
 
-print("\n\nProcessing dependencies")
+output_file.write("\n\nProcessing dependencies\n")
 project.process_dependencies()
-print("\nDependency builders after processing dependencies:")
-print(project.depbuilders)
+output_file.write("\nDependency builders after processing dependencies:\n")
+output_file.write(str(project.depbuilders))
 
-print("\n\nSearching Dependencies Recursively")
+output_file.write("\n\nSearching Dependencies Recursively")
 project.search_deps_recursive()
-print("\nDependency builders after searching dependencies recursively:")
-print(project.depbuilders)
+output_file.write("\nDependency builders after searching dependencies recursively:\n")
+output_file.write(str(project.depbuilders))
 
 if project.lib_ldf_mode.startswith("deep"):
     search_files = project.get_search_files()
-print("\n\nFound %d files to search" % len(search_files))
-print(json.dumps(search_files, indent=2))
-print()
+output_file.write("\n\nFound %d files to search\n" % len(search_files))
+output_file.write(json.dumps(search_files, indent=2))
+output_file.write("")
 
 lib_inc_map = {}
 for inc in project.get_implicit_includes(search_files):
     inc_path = inc.get_abspath()
-    print("implicit include:")
-    print(inc_path)
+    output_file.write("implicit include:")
+    output_file.write(inc_path)
     for lb in project.env.GetLibBuilders():
         if inc_path in lb:
             if lb not in lib_inc_map:
@@ -100,8 +106,9 @@ for inc in project.get_implicit_includes(search_files):
             lib_inc_map[lb].append(inc_path)
             break
 
-print("lib_inc_map:")
-print(json.dumps(lib_inc_map, indent=2))
+output_file.write("lib_inc_map:")
+output_file.write(json.dumps(lib_inc_map, indent=2))
 
+output_file.close()
 
 sys.exit()
