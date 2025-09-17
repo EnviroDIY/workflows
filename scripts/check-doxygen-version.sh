5aabf9120c4e9a151d8935b125d4d006950e8014
Sara Damiano <sdamiano@stroudcenter.org>
2025-09-17 11:45:14 -0400

    Output beautification
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- scripts/check-doxygen-version.sh
+++ scripts/check-doxygen-version.sh
@@ -5,7 +5,7 @@
 # GRAPHVIZ_VERSION=11.0.0
 
 # Set options,
-set -e # Exit with nonzero exit code if anything fails
+set +e
 if [ "$RUNNER_DEBUG" = "1" ]; then
     echo "Enabling debugging!"
     set -v # Prints shell input lines as they are read.
@@ -13,17 +13,24 @@ if [ "$RUNNER_DEBUG" = "1" ]; then
 fi
 
 doxygen -v
+result_code=${PIPESTATUS[0]}
+if [ "$result_code" -ne "0" ] && [ "$status" -eq "0" ]; then
+    echo "::notice::Doxygen is not installed!"
+    exit 0;
+fi
+
+set -e
 installed_doxygen=$(doxygen -v | cut -d ' ' -f1)
 echo "$installed_doxygen"
 echo "$installed_doxygen" | cut -d ' ' -f1
 installed_doxygen=$(echo "$installed_doxygen" | cut -d ' ' -f1)
-echo "Current Doxygen version is ${installed_doxygen}"
+echo "\e[36mCurrent Doxygen version is ${installed_doxygen}\e[0m"
 echo "Requested Doxygen version was ${DOXYGEN_VERSION}"
 echo "installed_doxygen=${installed_doxygen}" >>$GITHUB_OUTPUT
 if [ "$installed_doxygen" = "$DOXYGEN_VERSION" ]; then
-    echo "::notice::The requested Doxygen version is already installed"
+    echo "::notice::The requested Doxygen version (${installed_doxygen}) is already installed"
     echo "correct_doxygen=true" >>$GITHUB_OUTPUT
 else
-    echo "::warning::The installed Doxygen version is different than requestsed"
+    echo "::warning::The installed Doxygen version is different than requested"
     echo "correct_doxygen=false" >>$GITHUB_OUTPUT
 fi
--- scripts/check-graphviz-version.sh
+++ scripts/check-graphviz-version.sh
@@ -5,7 +5,7 @@
 # GRAPHVIZ_VERSION=11.0.0
 
 # Set options,
-set -e # Exit with nonzero exit code if anything fails
+set +e
 if [ "$RUNNER_DEBUG" = "1" ]; then
     echo "Enabling debugging!"
     set -v # Prints shell input lines as they are read.
@@ -13,17 +13,24 @@ if [ "$RUNNER_DEBUG" = "1" ]; then
 fi
 
 installed_graphviz_full=$(dot -V 2>&1 | cut -d ' ' -f5)
+result_code=${PIPESTATUS[0]}
+if [ "$result_code" -ne "0" ] && [ "$status" -eq "0" ]; then
+    echo "::notice::GraphViz is not installed!"
+    exit 0;
+fi
+
+set -e
 echo "Full version of GraphViz ${installed_graphviz_full}"
 installed_graphviz=$(echo "$installed_graphviz_full" | sed "s/dot - graphviz version //g")
 installed_graphviz=$(echo "$installed_graphviz" | sed -r "s/ \(\d+\)//g")
 echo "Substring of ${installed_graphviz}"
-installed_graphviz="$(echo -e "${installed_graphviz}" | tr -d '[:space:]')"
+installed_graphviz="$(echo "${installed_graphviz}" | tr -d '[:space:]')"
 echo "Current GraphViz version is ${installed_graphviz}"
 echo "The requested GraphViz version was ${GRAPHVIZ_VERSION}"
 if [ "$installed_graphviz" = "$GRAPHVIZ_VERSION" ]; then
-    echo "::notice::The requested GraphViz version is already installed"
+    echo "::notice::The requested GraphViz version (${installed_graphviz})is already installed"
     echo "correct_graphviz=true" >>$GITHUB_OUTPUT
 else
-    echo "::warning::The installed GraphViz version is different than requestsed"
+    echo "::warning::The installed GraphViz version is different than requested"
     echo "correct_graphviz=false" >>$GITHUB_OUTPUT
 fi
