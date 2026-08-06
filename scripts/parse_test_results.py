38d949e09040fb28b97715e964bdbba36976845e
Sara Damiano <sdamiano@stroudcenter.org>
2026-08-05 21:26:40 -0400

    Fix rounding
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- scripts/parse_test_results.py
+++ scripts/parse_test_results.py
@@ -194,18 +194,18 @@ for log_file in pio_logs + acli_logs:
 df = pd.DataFrame(log_results)
 df["flash_percent"] = df.apply(
     lambda row: (
-        (row["flash_used"] / row["flash_total"] * 100).round()
-        if row["flash_total"]
-        else None
+        (row["flash_used"] / row["flash_total"] * 100) if row["flash_total"] else None
     ),
     axis=1,
 )
+df["flash_percent"] = df["flash_percent"].round(1)
 df["ram_percent"] = df.apply(
     lambda row: (
-        (row["ram_used"] / row["ram_total"] * 100).round() if row["ram_total"] else None
+        (row["ram_used"] / row["ram_total"] * 100) if row["ram_total"] else None
     ),
     axis=1,
 )
+df["ram_percent"] = df["ram_percent"].round(1)
 df["success"] = df["success"].fillna(2).astype(int)
 df["success"] = df["success"].replace({2: "", 1: ":heavy_check_mark:", 0: ":x:"})
 df = df.sort_values(
